e53ddc538be8b6dccdf258a60a3d07693055588e
DAW <DAW@DESKTOP-U4FTC0L>
2023-05-12 13:25:04 +0200

    Commit de la rama

---
+++ Proyecto1/src/Paquete2/Rama1.java
@@ -0,0 +1,8 @@
+package Paquete2;
+
+public class Rama1 {
+	
+	public static void main(String args[]) {	
+		System.out.println("Esta es la primera línea de código de mi rama auxiliar");
+	}
+}
--- Proyecto1/src/paquete1/Principal.java
+++ Proyecto1/src/paquete1/Principal.java
@@ -4,5 +4,6 @@ public class Principal {
 	
 	public static void main(String args[]) {		
 		System.out.println("Control de versiones con Git");
+		System.out.println("Ejemplo 2 de control de versiones con Git");
 	}
 }
